cde58fafa683e5e23cf7d7c1028fa36d761d99ef
Morten Jansrud <morten.jansrud@nav.no>
2020-05-15 13:29:00 +0200

    Bruk originalUrl

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ server.get(`${baseUrl}/internal/isAlive|isReady`, (req, res) =>
 
 // Match everything except internal og static
 server.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) => {
-  const language = req.headers.host.indexOf("/en") !== -1 ? "engelsk" : "norsk";
+  const language = req.originalUrl.indexOf("/en") !== -1 ? "engelsk" : "norsk";
   const subdomain = req.headers.host.split(".")[0];
   const namespace = subdomain !== "www" ? subdomain.split("-")[1] : "p";
   getDecorator(namespace, language)
