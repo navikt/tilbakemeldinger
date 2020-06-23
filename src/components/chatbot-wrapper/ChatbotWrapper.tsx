0f8a4b8fe37edbb08f73f49f31e5c815c695fb7a
anders <anders@nomerstad.com>
2020-06-23 16:00:06 +0200

    Fikser chatbot-ikon overlap med footer-elementer

--- src/components/chatbot-wrapper/ChatbotWrapper.tsx
+++ src/components/chatbot-wrapper/ChatbotWrapper.tsx
@@ -1,5 +1,5 @@
 import NavChatbot from "@navikt/nav-chatbot";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Normaltekst } from "nav-frontend-typografi";
 import { openChatbot, setCallbackOnChatbotOpen } from "./ChatbotUtils";
 
@@ -12,9 +12,14 @@ type Props = {
 
 const cssPrefix = "chatbot-wrapper";
 
+const getElementTopPosition = (element: HTMLElement) => {
+  return element.getBoundingClientRect().bottom + window.pageYOffset || 0;
+};
+
 export const ChatbotWrapper = ({ customerKey, queueKey, configId, onOpen }: Props) => {
   const isOpenFromStorage = sessionStorage.getItem("chatbot-frida_apen");
   const [chatbotOpened, setChatbotOpened] = useState(isOpenFromStorage === "true");
+  const chatbotRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (chatbotOpened) {
@@ -24,6 +29,38 @@ export const ChatbotWrapper = ({ customerKey, queueKey, configId, onOpen }: Prop
     } else {
       setCallbackOnChatbotOpen(() => setChatbotOpened(true));
     }
+
+    const chatbotElement = chatbotRef.current;
+
+    if (!chatbotElement) {
+      return;
+    }
+
+    const element = document.getElementsByClassName("chatbot-wrapper")[0] as HTMLElement;
+
+    if (!element) {
+      return;
+    }
+
+    const footerBottom =
+      document.getElementsByClassName("menylinje-bottom")[0] as HTMLElement;
+
+    if (!footerBottom) {
+      return;
+    }
+
+    const scrollHandler = () => {
+      const scrollOffsetBottom = window.pageYOffset + window.innerHeight;
+      const chatbotOffset = getElementTopPosition(chatbotElement);
+      const footerOffset = getElementTopPosition(footerBottom);
+      if (scrollOffsetBottom > footerOffset) {
+
+      } else {
+      }
+    };
+
+    document.addEventListener("scroll", scrollHandler);
+    return () => document.removeEventListener("scroll", scrollHandler);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [chatbotOpened]);
 
@@ -31,6 +68,7 @@ export const ChatbotWrapper = ({ customerKey, queueKey, configId, onOpen }: Prop
     <div
       className={cssPrefix}
       onClick={() => openChatbot(setChatbotOpened)}
+      ref={chatbotRef}
     >
       <div className={`${cssPrefix}__visual ${chatbotOpened ? `${cssPrefix}__visual--open` : ""}`}>
         <Normaltekst className={`${cssPrefix}__text`}>
