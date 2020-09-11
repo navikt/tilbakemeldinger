1d59329188a0f5016db2e16093bb4a49cbb34ddd
anders-nom <anders@nomerstad.com>
2020-09-11 11:58:15 +0200

    Forsøker å fikse chatbot styling

--- src/index.tsx
+++ src/index.tsx
@@ -47,7 +47,7 @@ const init = async () => {
 
     // Execute client.js
     var script = document.createElement("script");
-    script.src = "https://www.nav.no/dekoratoren/client.js";
+    script.src = "http://localhost:8088/dekoratoren/client.js";
     document.body.appendChild(script);
   }
 
