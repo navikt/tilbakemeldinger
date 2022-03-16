a1293166cdc5c51db66bc938779895378e701273
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-03-16 15:07:29 +0100

    Fjernet parentes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const baseUrl = "/person/kontakt-oss/tilbakemeldinger";
 // Parse application/json
 server.use(express.json());
 server.use(baseUrl, express.static(buildPath, { index: false }));
-server().use(cookies());
+server.use(cookies());
 server.get(`${baseUrl}/internal/isAlive|isReady`, (req, res) =>
   res.sendStatus(200)
 );
