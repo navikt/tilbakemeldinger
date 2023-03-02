14e7b17efe6f6a0a3a7eb9e4d44ee2adc0e5c0ac
perolavm <per.olav.mariussen@nav.no>
2023-03-02 09:12:32 +0100

    Refaktor LoginModal for å støtte skjermlesere bedre

--- src/pages/tilbakemeldinger/service-klage/login-modal/LoginModal.tsx
+++ src/pages/tilbakemeldinger/service-klage/login-modal/LoginModal.tsx
@@ -1,48 +1,56 @@
 import React from "react";
 import Environment from "Environments";
 import { FormattedMessage } from "react-intl";
-import { BodyLong, Button, Heading } from "@navikt/ds-react";
+import {BodyLong, Button, Heading, Modal} from "@navikt/ds-react";
 
 const { loginUrl } = Environment();
 
 type Props = {
-  closeFunc: () => void;
+    open: boolean;
+    closeFunc: () => void;
 };
 
-const LoginModal = ({ closeFunc }: Props) => {
+const LoginModal = ({ open, closeFunc }: Props) => {
   return (
-    <div role="region" className="login-modal">
-      <Heading level="2" size="small" className="login-modal__title">
-        <FormattedMessage
-          id="tilbakemeldinger.serviceklage.login.overskrift"
-        />
-      </Heading>
-      <BodyLong spacing={true} className="login-modal__info">
-        <FormattedMessage
-          id="tilbakemeldinger.serviceklage.login.beskrivelse"
-          values={{ br: () => <br /> }}
-        />
-      </BodyLong>
-      <div className="login-modal__buttons">
-        <Button
-          variant="primary"
-          as="a"
-          href={`${loginUrl}?redirect=${window.location.href}`}
-        >
-          <FormattedMessage id="tilbakemeldinger.serviceklage.login.knapp" />
-        </Button>
-
-        <Button
-          variant="tertiary"
-          onClick={(e) => {
-            e.preventDefault();
-            closeFunc();
-          }}
-        >
-          <FormattedMessage id="tilbakemeldinger.serviceklage.login.knapp.fortsettuten" />
-        </Button>
-      </div>
-    </div>
+      <Modal
+          aria-labelledby="login-modal-heading"
+          open={open}
+          onClose={closeFunc}
+      >
+          <Modal.Content>
+             <div role="region" className="login-modal">
+              <Heading level="2" id="login-modal-heading" size="small" className="login-modal__title">
+                <FormattedMessage
+                  id="tilbakemeldinger.serviceklage.login.overskrift"
+                />
+              </Heading>
+              <BodyLong spacing={true} className="login-modal__info">
+                <FormattedMessage
+                  id="tilbakemeldinger.serviceklage.login.beskrivelse"
+                  values={{ br: () => <br /> }}
+                />
+              </BodyLong>
+              <div className="login-modal__buttons">
+                <Button
+                  variant="primary"
+                  as="a"
+                  href={`${loginUrl}?redirect=${window.location.href}`}
+                >
+                  <FormattedMessage id="tilbakemeldinger.serviceklage.login.knapp" />
+                </Button>
+                <Button
+                  variant="tertiary"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    closeFunc();
+                  }}
+                >
+                  <FormattedMessage id="tilbakemeldinger.serviceklage.login.knapp.fortsettuten" />
+                </Button>
+              </div>
+            </div>
+          </Modal.Content>
+      </Modal>
   );
 };
 
