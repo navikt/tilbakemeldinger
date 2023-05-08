3a447f7cee9f8b3de38e29b9a732c52da776a728
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-05-08 09:05:51 +0200

    Fikset isDev-variabel

--- src/Environments.ts
+++ src/Environments.ts
@@ -2,7 +2,7 @@ import { forsidePath } from "./Config";
 
 const Environment = () => {
   const host = window.location.host;
-  const isDev = host.startsWith("www.dev");
+  const isDev = host.startsWith("www.intern.dev");
   const baseAppPath = `${forsidePath}`;
 
   // Localhost
