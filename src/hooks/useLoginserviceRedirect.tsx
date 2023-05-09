e1d8059dc5408f396a517087052536002db705b4
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-05-09 15:49:34 +0200

    useEffect i hook

--- src/hooks/useLoginserviceRedirect.tsx
+++ src/hooks/useLoginserviceRedirect.tsx
@@ -1,14 +1,19 @@
 import { useStore } from '../providers/Provider';
 import Environments from '../Environments';
+import { useEffect } from 'react';
 
 export const useLoginserviceRedirect = () => {
     const [{ auth }] = useStore();
     const { loginUrl } = Environments();
 
-    // Redirect til loginservice hvis innlogget med wonderwall
-    if (auth.authenticated && !hasLoginserviceToken()) {
-        window.location.assign(`${loginUrl}?redirect=${window.location.href}`);
-    }
+    useEffect(() => {
+        // Redirect til loginservice hvis innlogget med wonderwall
+        if (auth.authenticated && !hasLoginserviceToken()) {
+            window.location.assign(
+                `${loginUrl}?redirect=${window.location.href}`
+            );
+        }
+    }, []);
 };
 
 const hasLoginserviceToken = () =>
