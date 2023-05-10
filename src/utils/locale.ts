3a5fbb8ccf1c9c9f5a6f178d1acbb2afc9888052
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-05-10 09:06:11 +0200

    Sikrer at url ikke har trailing backslash pga rigid allow list i loginservice

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -6,7 +6,9 @@ export const validLocales: Locale[] = ['nb', 'en', 'nn']; // :(
 export const defaultLocale = 'nb' as Locale;
 
 export const localePath = (path: string, locale: Locale) =>
-    `${paths.kontaktOss.forside}/${locale}${path}`;
+    `${paths.kontaktOss.forside}/${locale}${
+        path.endsWith('/') ? path.slice(0, -1) : path
+    }`;
 
 const isLocale = (str: string): str is Locale =>
     validLocales.includes(str as Locale);
@@ -16,12 +18,14 @@ export const setNewLocale = (
     dispatch: (action: Action) => void
 ) => {
     // TODO: bruk regex?
-    const subSegments = window.location.pathname
-        .split(paths.kontaktOss.forside)[1]
-        .split('/')
-        .slice(2)
-        .join('/');
-    const newUrl = `${paths.kontaktOss.forside}/${locale}/${subSegments}`;
+    const subSegments =
+        '/' +
+        window.location.pathname
+            .split(paths.kontaktOss.forside)[1]
+            .split('/')
+            .slice(2)
+            .join('/');
+    const newUrl = localePath(subSegments, locale);
 
     window.history.pushState({}, '', newUrl);
     dispatch({ type: 'SETT_LOCALE', payload: locale });
