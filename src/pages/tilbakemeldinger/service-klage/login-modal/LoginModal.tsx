3b91ef9f1f6f680c8b9c6aa58dcd909752213175
Bjørn Harbo Dahle <bdahle@hey.com>
2023-10-31 14:06:05 +0100

    rydd i LoginModal

--- src/pages/tilbakemeldinger/service-klage/login-modal/LoginModal.tsx
+++ src/pages/tilbakemeldinger/service-klage/login-modal/LoginModal.tsx
@@ -18,41 +18,34 @@ const LoginModal = ({ open, closeFunc }: Props) => {
             onClose={closeFunc}
         >
             <Modal.Body>
-                <div className="login-modal">
-                    <Heading
-                        level="2"
-                        id="login-modal-heading"
-                        size="small"
-                        className="login-modal__title"
-                    >
-                        <FormattedMessage id="tilbakemeldinger.serviceklage.login.overskrift" />
-                    </Heading>
-                    <BodyLong spacing={true} className="login-modal__info">
-                        <FormattedMessage
-                            id="tilbakemeldinger.serviceklage.login.beskrivelse"
-                            values={{ br: () => <br /> }}
-                        />
-                    </BodyLong>
-                    <div className="login-modal__buttons">
-                        <Button
-                            variant="primary"
-                            as="a"
-                            href={`${loginUrl}?redirect=${window.location.href}`}
-                        >
-                            <FormattedMessage id="tilbakemeldinger.serviceklage.login.knapp" />
-                        </Button>
-                        <Button
-                            variant="tertiary"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                closeFunc();
-                            }}
-                        >
-                            <FormattedMessage id="tilbakemeldinger.serviceklage.login.knapp.fortsettuten" />
-                        </Button>
-                    </div>
-                </div>
+                <Heading level="2" id="login-modal-heading" size="small">
+                    <FormattedMessage id="tilbakemeldinger.serviceklage.login.overskrift" />
+                </Heading>
+                <BodyLong>
+                    <FormattedMessage
+                        id="tilbakemeldinger.serviceklage.login.beskrivelse"
+                        values={{ br: () => <br /> }}
+                    />
+                </BodyLong>
             </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant="primary"
+                    as="a"
+                    href={`${loginUrl}?redirect=${window.location.href}`}
+                >
+                    <FormattedMessage id="tilbakemeldinger.serviceklage.login.knapp" />
+                </Button>
+                <Button
+                    variant="tertiary"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        closeFunc();
+                    }}
+                >
+                    <FormattedMessage id="tilbakemeldinger.serviceklage.login.knapp.fortsettuten" />
+                </Button>
+            </Modal.Footer>
         </Modal>
     );
 };
