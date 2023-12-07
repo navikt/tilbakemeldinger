63a651a8c34ad11b44e76602616318016b01d7bc
Bjørn Harbo Dahle <bdahle@hey.com>
2023-12-07 12:51:26 +0100

    leg til global.d.ts i src

--- src/global.d.ts
+++ src/global.d.ts
@@ -7,3 +7,16 @@ declare global {
         };
     }
 }
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            VITE_APP_BASEPATH: string;
+            APP_PORT: string;
+            ENV: 'prod' | 'dev' | 'localhost';
+            NODE_ENV: 'development' | 'production';
+        }
+    }
+}
+
+export {};
