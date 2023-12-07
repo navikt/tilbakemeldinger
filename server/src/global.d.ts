203bab123b07ca314b62d873b1ea87f68371abbe
Bjørn Harbo Dahle <bdahle@hey.com>
2023-12-07 12:51:26 +0100

    leg til global.d.ts i src

--- server/src/global.d.ts
+++ server/src/global.d.ts
@@ -1,9 +1,9 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            ENV: 'prod' | 'dev' | 'localhost';
             VITE_APP_BASEPATH: string;
             APP_PORT: string;
+            ENV: 'prod' | 'dev' | 'localhost';
             NODE_ENV: 'development' | 'production';
         }
     }
