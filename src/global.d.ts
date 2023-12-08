13466e8f42ba7432d2ab1bc34a7219c90323f63d
Bjørn Harbo Dahle <bdahle@hey.com>
2023-12-08 16:08:52 +0100

    rydd i diverse

--- src/global.d.ts
+++ src/global.d.ts
@@ -12,9 +12,7 @@ declare global {
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
-            VITE_APP_BASEPATH: string;
             APP_PORT: string;
-            ENV: 'prod' | 'dev' | 'localhost';
             NODE_ENV: 'development' | 'production';
         }
     }
