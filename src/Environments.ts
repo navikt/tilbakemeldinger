4ee4e0cf34ec58c6d9f2cc9b34c5496c6ac8c769
Bjørn Harbo Dahle <bdahle@hey.com>
2023-12-14 09:44:39 +0100

    samle felles URLer

--- src/Environments.ts
+++ src/Environments.ts
@@ -3,43 +3,43 @@ const Environment = () => {
     const isDev = import.meta.env.VITE_ENV === 'dev';
     const origin = import.meta.env.VITE_APP_ORIGIN;
 
+    const commonUrls = {
+        baseUrl: origin,
+        appUrl: `${origin}/person/kontakt-oss/tilbakemeldinger`,
+        personInfoApiUrl: `${origin}/person/personopplysninger-api`,
+    };
+
     if (isLocal) {
         return {
-            baseUrl: origin,
-            appUrl: `${origin}/person/kontakt-oss/tilbakemeldinger`,
             authUrl: `${origin}/person/nav-dekoratoren-api/auth`,
-            personInfoApiUrl: `${origin}/person/personopplysninger-api`,
             mineSakerUrl: 'https://www.intern.dev.nav.no/mine-saker',
             loginUrl: `${origin}/personbruker-api/local/cookie`,
             klageUrl: 'https://www.ekstern.dev.nav.no/klage',
             klageUrlEn: 'https://www.ekstern.dev.nav.no/klage/en',
+            ...commonUrls,
         };
     }
 
     if (isDev) {
         return {
-            baseUrl: origin,
-            appUrl: `${origin}/person/kontakt-oss/tilbakemeldinger`,
             authUrl:
                 'https://www.ekstern.dev.nav.no/person/nav-dekoratoren-api/auth',
-            personInfoApiUrl: `${origin}/person/personopplysninger-api`,
             mineSakerUrl: `${origin}/mine-saker`,
             loginUrl: 'https://login.ekstern.dev.nav.no/oauth2/login',
             klageUrl: 'https://www.ekstern.dev.nav.no/klage',
             klageUrlEn: 'https://www.ekstern.dev.nav.no/klage/en',
+            ...commonUrls,
         };
     }
 
     //isProd
     return {
-        baseUrl: origin,
-        appUrl: `${origin}/person/kontakt-oss/tilbakemeldinger`,
         authUrl: `${origin}/person/nav-dekoratoren-api/auth`,
-        personInfoApiUrl: `${origin}/person/personopplysninger-api`,
         mineSakerUrl: 'https://person.nav.no/mine-saker',
         loginUrl: 'https://login.nav.no/oauth2/login',
         klageUrl: 'https://www.nav.no/klage',
         klageUrlEn: 'https://www.nav.no/klage/en',
+        ...commonUrls,
     };
 };
 
