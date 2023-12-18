0015602d49a073a78d1ee0d0e455af915a4054fd
Bjørn Harbo Dahle <bdahle@hey.com>
2023-12-18 12:42:47 +0100

    test injectDecoratorServerSide med parametere

--- server/src/site/ssr/templateBuilder.ts
+++ server/src/site/ssr/templateBuilder.ts
@@ -1,6 +1,11 @@
 import path from 'path';
 import fs from 'fs';
 import { injectWithDecorator } from '../../utils/decorator';
+import { injectDecoratorServerSide } from '@navikt/nav-dekoratoren-moduler/ssr';
+import {
+    DecoratorEnvProps,
+    DecoratorParams,
+} from '@navikt/nav-dekoratoren-moduler/ssr';
 
 const templatePath =
     process.env.NODE_ENV === 'development'
@@ -19,4 +24,44 @@ export const buildHtmlTemplate = async () => {
     }
 
     return templateWithDecorator;
+};
+
+const decoratorEnv = 'prod';
+const decoratorLocalUrl = process.env.DECORATOR_LOCAL_URL;
+
+const envProps: DecoratorEnvProps =
+    // decoratorEnv === 'localhost'
+    //     ? {
+    //           env: decoratorEnv,
+    //           localUrl: 'http://localhost:8100',
+    //       }
+    //     :
+    { env: decoratorEnv };
+
+export const getTemplateWithDecorator = async (locale: any) => {
+    // const params = getDecoratorParams(locale, 'testUrl');
+    const params: DecoratorParams = {
+        // context: 'privatperson',
+        language: 'en',
+        breadcrumbs: [
+            {
+                url: `testURL`,
+                title: 'testTitle',
+            },
+            {
+                url: '/',
+                title: 'testTitle2',
+            },
+        ],
+        availableLanguages: [
+            { locale: 'nb', handleInApp: true },
+            { locale: 'en', handleInApp: true },
+        ],
+    };
+
+    return injectDecoratorServerSide({
+        env: 'prod',
+        filePath: templatePath,
+        params,
+    });
 };
