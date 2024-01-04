e594c996587f752aabf9dceba3535390fafe63ed
Bjørn Harbo Dahle <bdahle@hey.com>
2024-01-04 11:53:49 +0100

    Revert "test basic Helmet"
    This reverts commit 6a83162d58212459f2c915843e395b59f011c84e.

--- src/components/metatags/MetaTags.tsx
+++ src/components/metatags/MetaTags.tsx
@@ -14,22 +14,30 @@ type Props = {
 };
 
 export const MetaTags = ({ path, titleId, descriptionId, children }: Props) => {
-    // const intl = useIntl();
-    // const [{ locale }] = useStore();
-    // const baseUrl = Environment().baseUrl;
-    // const title = intl.formatMessage({ id: titleId });
+    const intl = useIntl();
+    const [{ locale }] = useStore();
+    const baseUrl = Environment().baseUrl;
+    const title = intl.formatMessage({ id: titleId });
 
-    // useEffect(() => {
-    //     logPageview(title);
-    // }, [title]);
+    useEffect(() => {
+        logPageview(title);
+    }, [title]);
 
     return (
         <Helmet>
-            {titleId && <title>{`www.nav.no`}</title>}
+            {titleId && <title>{`${title} - www.nav.no`}</title>}
             {descriptionId && (
-                <meta name="description" content={descriptionId} />
+                <meta
+                    name="description"
+                    content={intl.formatMessage({ id: descriptionId })}
+                />
             )}
-            {(path || path === '') && <link rel="canonical" href={path} />}
+            {(path || path === '') && (
+                <link
+                    rel="canonical"
+                    href={`${baseUrl}${localePath(path, locale)}`}
+                />
+            )}
             {children}
         </Helmet>
     );
