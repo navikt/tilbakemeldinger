1703f027b87f8958fd38ee7c361671bf705ea785
Bjørn Harbo Dahle <bdahle@hey.com>
2024-01-05 10:37:19 +0100

    fjern helmetprovider fra SSR (forårsaker tom maincontent)

--- src/main-server.tsx
+++ src/main-server.tsx
@@ -2,21 +2,16 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { AppRoot } from './index';
-import { HelmetProvider } from 'react-helmet-async';
 
 export const render = (url: string) => {
     console.log('Rendering url:', url, import.meta.env.VITE_APP_BASEPATH);
 
-    const helmetContext = {};
-
     return renderToString(
-        // <HelmetProvider context={helmetContext}>
         <StaticRouter
             basename={import.meta.env.VITE_APP_BASEPATH}
             location={url}
         >
             <AppRoot />
         </StaticRouter>
-        // </HelmetProvider>
     );
 };
