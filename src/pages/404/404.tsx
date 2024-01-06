ae49e4db3647926a6a9d1e07d8ec12b1e10789d9
Bjørn Harbo Dahle <bdahle@hey.com>
2024-01-06 12:24:32 +0100

    bare bruk Helmet i klienten

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { paths } from 'common/paths';
 import { FormattedMessage } from 'react-intl';
 import { Heading, Link } from '@navikt/ds-react';
 import { Helmet } from 'react-helmet-async';
 import style from './404.module.scss';
 
-const NotFound = () => (
-    <>
-        <Helmet>
-            <meta name="robots" content="noindex" />
-        </Helmet>
-        <div className={style.container}>
-            <Heading size={'medium'} level={'2'}>
-                <FormattedMessage id={'feil.404'} />
-            </Heading>
-            <Link href={paths.kontaktOss.forside}>
-                <FormattedMessage id={'feil.lenke'} />
-            </Link>
-        </div>
-    </>
-);
+const NotFound = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    return (
+        <>
+            {isClient && (
+                <Helmet>
+                    <meta name="robots" content="noindex" />
+                </Helmet>
+            )}
+            <div className={style.container}>
+                <Heading size={'medium'} level={'2'}>
+                    <FormattedMessage id={'feil.404'} />
+                </Heading>
+                <Link href={paths.kontaktOss.forside}>
+                    <FormattedMessage id={'feil.lenke'} />
+                </Link>
+            </div>
+        </>
+    );
+};
 
 export default NotFound;
