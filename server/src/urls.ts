c84746973057b07df56183137f1f826919afddd0
Bjørn Harbo Dahle <bdahle@hey.com>
2024-01-07 12:14:31 +0100

    stopp feilmledinger av typen "No translation found for key: seo.isAlive.description..."

--- server/src/urls.ts
+++ server/src/urls.ts
@@ -2,4 +2,10 @@ export const URLs = {
     navno404: 'https://www.nav.no/404',
     norg2Path: '/norg2/api/v1/enhet?enhetStatusListe=AKTIV',
     norg2Origin: process.env.NORG2_ORIGIN,
+    allPages: [
+        'tilbakemeldinger',
+        'serviceklage',
+        'feil-og-mangler',
+        'ros-til-nav',
+    ],
 };
