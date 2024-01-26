9894162fc58e323951d449a5ab1b13e0b409d9b5
Bjørn Harbo Dahle <bdahle@hey.com>
2024-01-26 10:01:27 +0100

    fjern ubrukte funksjoner for head tags på server

--- server/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import { URLs } from '../urls';
-
-export const getLastSegmentFromUrl = (url: string) => {
-    const segments = url.split('/');
-    return segments.pop() ?? '';
-};
-
-export const checkIfPage = (url: string) =>
-    URLs.allPages.includes(getLastSegmentFromUrl(url));
