32b1ae1d27b700abbc2c906062b7674dde30f1e9
Bjørn Harbo Dahle <bdahle@hey.com>
2024-02-05 14:20:10 +0100

    Revert "test logg full feilmelding"
    This reverts commit 1519bb3f6b4e577093150fa247bd5353859d5d6b.

--- server/src/utils/errorHandlers.ts
+++ server/src/utils/errorHandlers.ts
@@ -27,15 +27,15 @@ export const setupErrorHandlers = async (expressApp: Express) => {
     const serverErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
         const { path } = req;
         const { status, stack } = err;
-        // const msg = stack?.split('\n')[0]; TODO reverser?
+        const msg = stack?.split('\n')[0];
         const statusCode = status || 500;
 
         if (statusCode < 500) {
-            console.log(`Invalid request to ${path}: ${statusCode} ${stack}`);
+            console.log(`Invalid request to ${path}: ${statusCode} ${msg}`);
             return notFoundHandler(req, res, next);
         }
 
-        console.error(`Server error on ${path}: ${statusCode} ${stack}`);
+        console.error(`Server error on ${path}: ${statusCode} ${msg}`);
 
         return res.status(statusCode).end();
     };
