8a6630e61b7201439cfea9e3d95739c2248a9540
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-02-08 11:34:55 +0100

    Reelle rate limits

--- server/src/api/setupApiRoutes.ts
+++ server/src/api/setupApiRoutes.ts
@@ -13,29 +13,27 @@ export const setupApiRoutes = async (router: Router) => {
     router.get('/fodselsnr', fodselsNrHandler);
     router.post(
         '/mottak/:path(ros|serviceklage|feil-og-mangler)',
-        ipRateLimit,
         globalRateLimit,
+        ipRateLimit,
         postToTilbakemeldingsmottakHandler
     );
     router.get('/enheter', enheterHandler);
 };
 
-const ipRateLimit = rateLimit({
-    windowMs: 30 * 60 * 1000, // 30 minutes
-    //max: 5,
-    max: 7,
-    standardHeaders: true,
-    message: 'Rate limit IP',
-});
-
 const globalRateLimit = rateLimit({
-    windowMs: 60 * 1000, // 1 minutes
-    //max: 100,
-    max: 3,
+    windowMs: 30 * 60 * 1000, // 30 minutes
+    max: 100,
     standardHeaders: true,
     keyGenerator: async (req) => {
         const accessToken = await getAccessToken(req);
         return accessToken || 'unauthenticated';
     },
     message: 'Rate limit',
+});
+
+const ipRateLimit = rateLimit({
+    windowMs: 30 * 60 * 1000, // 30 minutes
+    max: 5,
+    standardHeaders: true,
+    message: 'Rate limit IP',
 });
