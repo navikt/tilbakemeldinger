3fb80873dd41fd3962d39e7eeee453ac8e4123f3
bjornhun <bjornar.hunshamar@nav.no>
2024-02-08 15:35:37 +0100

    Rate limiting (#688)
    Lagt på rate limiting

--- server/src/api/setupApiRoutes.ts
+++ server/src/api/setupApiRoutes.ts
@@ -4,6 +4,8 @@ import { isAliveHandler } from './routes/isAlive/isAliveHandler';
 import { fodselsNrHandler } from './routes/fodselsNr/fodselsNrHandler';
 import { postToTilbakemeldingsmottakHandler } from './routes/postToTilbakemeldingsmottak/postToTilbakemeldingsmottakHandler';
 import { enheterHandler } from './routes/enheter/enheterHandler';
+import { rateLimit } from 'express-rate-limit';
+import { getAccessToken } from '../utils/auth/common';
 
 export const setupApiRoutes = async (router: Router) => {
     router.get('/internal/isAlive', isAliveHandler);
@@ -11,7 +13,35 @@ export const setupApiRoutes = async (router: Router) => {
     router.get('/fodselsnr', fodselsNrHandler);
     router.post(
         '/mottak/:path(ros|serviceklage|feil-og-mangler)',
+        globalRateLimit,
+        ipRateLimit,
         postToTilbakemeldingsmottakHandler
     );
     router.get('/enheter', enheterHandler);
 };
+
+const globalRateLimit = rateLimit({
+    windowMs: 30 * 60 * 1000, // 30 minutes
+    max: 100,
+    standardHeaders: true,
+    keyGenerator: async (req) => {
+        const accessToken = await getAccessToken(req);
+        return accessToken || 'unauthenticated';
+    },
+    message: 'Rate limit',
+});
+
+const ipRateLimit = rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 minutes
+    max: 5,
+    standardHeaders: true,
+    keyGenerator: (req) => {
+        return (
+            req.ip ||
+            req.get('x-real-ip') ||
+            req.get('x-forwarded-for') ||
+            'default'
+        );
+    },
+    message: 'Rate limit IP',
+});
