733434043467e4265570ff519224a7213d58a997
bjornhun <bjornar.hunshamar@nav.no>
2024-02-08 15:35:37 +0100

    Rate limiting (#688)
    Lagt på rate limiting

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,3 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '../.env' });
-
 import express from 'express';
 import compression from 'compression';
 import { setupSiteRoutes } from './site/setupSiteRoutes.js';
