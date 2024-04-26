726da10de04a41f004643b70ac57eb53c664e284
Anders Nomerstad <anders@nomerstad.com>
2024-04-26 12:52:29 +0200

    Filtrerer ut data fra norg2 enheter-api som ikke benyttes av appen. Oppdaterer mock-data.

--- server/src/api/routes/enheter/enheterHandler.ts
+++ server/src/api/routes/enheter/enheterHandler.ts
@@ -1,9 +1,27 @@
 import { RequestHandler } from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 import { URLs } from '../../../urls';
+import { Enhet } from '../../../../../common/enhet';
 
-export const enheterHandler: RequestHandler = createProxyMiddleware({
-    target: URLs.norg2Origin,
-    pathRewrite: () => URLs.norg2Path,
-    changeOrigin: true,
-});
+const NORG2_API_URL = `${URLs.norg2Origin}${URLs.norg2Path}`;
+
+const transformEnhet = (enhetRaw: Record<string, unknown> & Enhet): Enhet => ({
+    enhetNr: enhetRaw.enhetNr,
+    status: enhetRaw.status,
+    navn: enhetRaw.navn,
+    type: enhetRaw.type
+})
+
+export const enheterHandler: RequestHandler = async (req, res) => {
+    const enheter = await fetch(NORG2_API_URL).then(norgRes => norgRes.json()).catch(e => {
+        console.error(`Error fetching enheter from norg2 - ${e}`);
+        return null;
+    });
+
+    if (!enheter || !Array.isArray(enheter)) {
+        return res.status(500).send('Lasting av enheter feilet');
+    }
+
+
+    return res.send(enheter.map(transformEnhet));
+
+};
